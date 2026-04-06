32a39e7d02d50d03d88f263d7fc38ee359640c37
Seesa-cz <pavel.skoupy@email.cz>
2026-04-06 18:54:04 +0200

    Add files via upload

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+REPO_URL="https://github.com/Seesa-cz/dotfiles.git"
+PLAYBOOK="ubuntu-desktop.yml"
+
+echo "=== Instalace závislostí ==="
+sudo apt-get install -y ansible git
+
+echo "=== Spuštění playbooku ==="
+ansible-pull -U "$REPO_URL" "$PLAYBOOK" --ask-become-pass
