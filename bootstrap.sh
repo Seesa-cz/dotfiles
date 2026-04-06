ef87e8a76ccf8ff28b1723a2d5fd1efb2947b631
Seesa-cz <pavel.skoupy@email.cz>
2026-04-06 22:32:53 +0200

    Edge remove

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ REPO_URL="https://github.com/Seesa-cz/dotfiles.git"
 PLAYBOOK="ubuntu-desktop.yml"
 
 echo "=== Instalace závislostí ==="
-sudo apt-get install -y ansible git
+sudo apt-get install -y ansible git curl
 
 echo "=== Spuštění playbooku ==="
 ansible-pull -U "$REPO_URL" "$PLAYBOOK" --ask-become-pass
